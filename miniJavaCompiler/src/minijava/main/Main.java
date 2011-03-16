50072b967489cd3385901639eab12b5947bcacc0
Plinio Sandes Dos Santos <ra063707@odie.lab.ic.unicamp.br>
2011-03-16 10:48:01 -0300

    Adicionando projeto eclipse. As configuracoes para rodar o projeto podem ser encontradas em:     http://www.comp.nus.edu.sg/~sethhetu/rooms/Tutorials/EclipseAndSableCC.html
    Jamais adicione arquivos da pasta bin ao projeto !!!
    
    Novo caminho para o arquivo de entrada do sableCC:
        src/miniJava.sablecc
    
    Projeto preparado para receber como argumento do
    compilador o arquivo:
        src/entrada.minijava

---
+++ miniJavaCompiler/src/minijava/main/Main.java
@@ -0,0 +1,36 @@
+package minijava.main;
+
+import minijava.lexer.Lexer;
+import minijava.node.Start;
+import minijava.parser.Parser;
+
+import java.io.*;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if (args.length > 0) {
+	         try {
+	            /* Form our AST */
+	            Lexer lexer = new Lexer (new PushbackReader(
+	               new FileReader(args[0])));
+	            Parser parser = new Parser(lexer);
+	            Start ast = parser.parse() ;
+	 
+	            /* Get our Interpreter going. */
+//	            Interpreter interp = new Interpreter () ;
+//	            ast.apply(interp) ;
+	         }
+	         catch (Exception e) {
+	            System.out.println (e) ;
+	         }
+	      } else {
+	         System.err.println("usage: java simpleAdder inputFile");
+	         System.exit(1);
+	      } 
+	}
+
+}
