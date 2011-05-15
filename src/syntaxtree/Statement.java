86c2dce0263009fdfd494ac57cf648a255f631c5
Suguro <rafaelsuguro@gmail.com>
2011-05-15 00:33:30 -0300

    deletando arquivos

--- src/syntaxtree/Statement.java
+++
@@ -1,9 +0,0 @@
-package syntaxtree;
-
-public abstract class Statement extends Absyn
-{
-	public Statement(int l, int r)
-	{
-		super(l, r);
-	}
-}
--- src/visitor/Visitable.java
+++
@@ -1,9 +0,0 @@
-package visitor;
-
-import syntaxtree.Type;
-
-public interface Visitable
-{
-	public void accept(Visitor v);
-	public Type accept(TypeVisitor v);
-}
