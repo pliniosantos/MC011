2fbf13bead0286336f5c9985847600a3772cfc37
Plinio Sandes Dos Santos <ra063707@ash.lab.ic.unicamp.br>
2011-05-18 12:54:42 -0300

    Versao do pp.sh para a P2

--- miniJavaCompiler/src/pp.sh
+++ miniJavaCompiler/src/pp.sh
@@ -2,15 +2,22 @@
 # Script para rodar os testes de MC011-P1
 #
 
-num=20
 dir=$1
 
+#numero de arquivos no diretorio
+num=$(ls $dir | wc -l)
+
+#percorre todos os arquivos
 for (( i=1 ; i <= $num ; i++ ))
 do
     if [ $i -lt 10 ]
     then
-        java minijava/main/Main $1/in/0$i.java > $1/out/0$i.out
+        #tem que adicionar um 0 no nome dos arquivos entre 1 e 9
+        java minijava/main/Main $1/in/0$i > $1/g15/0$i.out
     else
-        java minijava/main/Main $1/in/$i.java > $1/out/$i.out
+        java minijava/main/Main $1/in/$i > $1/g15/$i.out
     fi
 done
+
+#concatena todos os arquivos .out em dir/gall/g15.out
+cat $1/g15/*.out > $1/gall/g15.out
