5c6e91e55cbbd3fcb866ab7a90f00956a4db0b34
Plinio Sandes dos Santos <plinio.sandes.dos.santos@gmail.com>
2011-05-18 18:15:52 -0300

    Novo pp.sh para P2

--- src/pp.sh
+++ src/pp.sh
@@ -5,19 +5,21 @@
 dir=$1
 
 #numero de arquivos no diretorio
-num=$(ls $dir | wc -l)
-
+num=$(ls $dir`/in` | wc -l)
+echo $num
 #percorre todos os arquivos
 for (( i=1 ; i <= $num ; i++ ))
 do
     if [ $i -lt 10 ]
     then
         #tem que adicionar um 0 no nome dos arquivos entre 1 e 9
-        java minijava/main/Main $1/in/0$i > $1/g15/0$i.out
+        java minijava/main/Main $1/in/0$i 2>&1 >/dev/null | tee $f > $1/g15/0$i.out
+	cat $1/g15/0$i.out >> $1/gall/g15.out
     else
-        java minijava/main/Main $1/in/$i > $1/g15/$i.out
+        java minijava/main/Main $1/in/$i 2>&1 >/dev/null | tee $f > $1/g15/$i.out
+	cat $1/g15/$i.out >> $1/gall/g15.out
     fi
 done
 
 #concatena todos os arquivos .out em dir/gall/g15.out
-cat $1/g15/*.out > $1/gall/g15.out
+#cat $1/g15/*.out > $1/gall/g15.out
